b664c4150073d75324688edd03a4f7ef65efd760
sussekl <sussekl@mail.uni-paderborn.de>
2025-11-14 13:05:13 +0100

    Minor changes for smaller run

--- job.sh
+++ job.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-#SBATCH -t 3:00:00
+#SBATCH --mail-type END,FAIL
+#SBATCH --mail-user sussekl@mail.uni-paderborn.de
+#SBATCH -t 10:00:00
 #SBATCH -N 1
-#SBATCH -n 3
+#SBATCH -n 1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=2G
-#SBATCH -J "Shapley_Experiment"
+#SBATCH --gres=gpu:a100:1
+#SBATCH -J "Shapley_Experiment_%a"
 #SBATCH -p gpu
-#SBATCH --gres=gpu:a100:3
 #SBATCH -A hpc-prf-wiki
+#SBATCH --array=0-2
 
 
+module load lang/Tkinter/3.9.6-GCCcore-11.2.0
 module load lang/Python/3.9.5-GCCcore-10.3.0
 module load system/CUDA/12.4.1
 source .venv/bin/activate
-srun python -m Evaluation.eval --dataset MOOC --explainer all &
-srun python -m Evaluation.eval --dataset Reddit --explainer all &
-srun python -m Evaluation.eval --dataset Wikipedia --explainer all &
-wait
+
+options=( "MOOC" "Reddit" "Wikipedia" )
+
+srun --ntasks=1 --cpus-per-task=16 --gres=gpu:a100:1 python -m Evaluation.eval --dataset ${options[$SLURM_ARRAY_TASK_ID]} --explainer all
