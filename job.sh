70ff66f3386b9b42ca9d693da8de703b1dce0698
sussekl <sussekl@mail.uni-paderborn.de>
2025-11-17 08:12:28 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -3,13 +3,13 @@
 #SBATCH --mail-user sussekl@mail.uni-paderborn.de
 #SBATCH -t 48:00:00
 #SBATCH -N 1
-#SBATCH -n 1
+#SBATCH --ntasks=1 
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:a100:1
 #SBATCH -J "Shapley_Experiment_%a"
 #SBATCH -p gpu
 #SBATCH -A hpc-prf-wiki
-#SBATCH --array=0-2
+#SBATCH --array=0-1
 
 
 module load lang/Tkinter/3.9.6-GCCcore-11.2.0
@@ -20,4 +20,5 @@ source /scratch/hpc-prf-wiki/sussekl/venv/bin/activate
 #options=( "MOOC" "Reddit" "Wikipedia" )
 options=( "MOOC" "Wikipedia" )
 
-srun --ntasks=1 --cpus-per-task=32 --gres=gpu:a100:1 python -m Evaluation.eval_no_prep --dataset ${options[$SLURM_ARRAY_TASK_ID]} --explainer all
+#srun --ntasks=1 --cpus-per-task=32 --gres=gpu:a100:1 python -m Evaluation.eval_no_prep --dataset ${options[$SLURM_ARRAY_TASK_ID]} --explainer all
+python -m Evaluation.eval_no_prep --dataset ${options[$SLURM_ARRAY_TASK_ID]} --explainer all
