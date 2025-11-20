1f5d349187fbaa270dd5ee585b8d004bc1e7aef9
sussekl <sussekl@mail.uni-paderborn.de>
2025-11-20 07:50:05 +0100

    Update jobs

---
+++ Jobs/job2 copy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --mail-type END,FAIL
+#SBATCH --mail-user sussekl@mail.uni-paderborn.de
+#SBATCH -t 24:00:00
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:a100:1
+#SBATCH -J "Shapley_Experiment_TGNN"
+#SBATCH -p gpu
+#SBATCH -A hpc-prf-wiki
+
+
+module load lang/Tkinter/3.9.6-GCCcore-11.2.0
+module load lang/Python/3.9.5-GCCcore-10.3.0
+module load system/CUDA/12.4.1
+source /scratch/hpc-prf-wiki/sussekl/venv/bin/activate
+
+python -m Evaluation.eval2 --dataset Reddit --explainer tgnnexplainer
