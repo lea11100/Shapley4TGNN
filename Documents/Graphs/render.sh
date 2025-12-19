52abb2924f5d1d381094095ec00c8583cf1a11c5
LSussek <sussekl@mail.uni-paderborn.de>
2025-12-19 11:35:45 +0100

    Update graph prints

--- Documents/Graphs/render.sh
+++ Documents/Graphs/render.sh
@@ -2,6 +2,6 @@ mkdir -p rendered_graphs
 
 for file in *.dot; do
     output="rendered_graphs/${file%.dot}.pdf"
-    dot -Tpdf "$file" -o "$output"
+    dot -Gmode=KK -Tpdf "$file" -o "$output"
     echo "Rendered $file to $output"
 done
